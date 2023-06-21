8ab2c6748afa2fb985d1a24873038f04d898cd70
hz <hz@hz.com>
2023-06-21 17:07:06 +0200

    Revert "intermediate commit(broken compilation) - moving all globals in one scope, prepare to unlink unneded *.o modules"
    This reverts commit 073107738bdcc4a59fdb91d4c456e786522be671.

--- run-all.sh
+++ run-all.sh
@@ -38,6 +38,7 @@ ${CLANG_BINARY} -target assigner \
     -I${HOME}/zkllvm/libs/stdlib/libcpp \
     -I${HOME}/zkllvm/libs/crypto3/algebra/include \
     -I${HOME}/zkllvm/build/include \
+    -I/usr/local/include \
     -I${HOME}/zkllvm/libs/blueprint/include \
     -I${HOME}/zkllvm/libs/crypto3/block/include -I${HOME}/zkllvm/libs/crypto3/codec/include -I${HOME}/zkllvm/libs/crypto3/containers/include -I${HOME}/zkllvm/libs/crypto3/hash/include -I${HOME}/zkllvm/libs/crypto3/kdf/include -I${HOME}/zkllvm/libs/crypto3/mac/include -I${HOME}/zkllvm/libs/crypto3/marshalling/core/include -I -I${HOME}/zkllvm/libs/crypto3/marshalling/algebra/include -I${HOME}/zkllvm/libs/crypto3/marshalling/multiprecision/include -I${HOME}/zkllvm/libs/crypto3/marshalling/zk/include -I${HOME}/zkllvm/libs/crypto3/math/include -I${HOME}/zkllvm/libs/crypto3/modes/include -I${HOME}/zkllvm/libs/crypto3/multiprecision/include -I${HOME}/zkllvm/libs/crypto3/passhash/include -I${HOME}/zkllvm/libs/crypto3/pbkdf/include -I${HOME}/zkllvm/libs/crypto3/pkmodes/include -I${HOME}/zkllvm/libs/crypto3/pkpad/include -I${HOME}/zkllvm/libs/crypto3/pubkey/include -I${HOME}/zkllvm/libs/crypto3/random/include -I${HOME}/zkllvm/libs/crypto3/stream/include -I${HOME}/zkllvm/libs/crypto3/vdf/include -I${HOME}/zkllvm/libs/crypto3/zk/include \
     -D__ZKLLVM__ \
