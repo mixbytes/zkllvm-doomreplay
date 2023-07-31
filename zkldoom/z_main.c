aea51b09a33e7e350de7c02045d976667bef8834
hz <hz@hz.com>
2023-07-31 12:44:58 +0200

    added Dijkstra algorithm testing

--- zkldoom/z_main.c
+++ zkldoom/z_main.c
@@ -16,6 +16,8 @@
 //	Main program, simply calls D_DoomMain high level loop.
 //
 
+
+/*
 //#include "config.h"
 
 #include "doomtype.h"
@@ -23,20 +25,20 @@
 #include "m_argv.h"
 
 
-#include <stdlib.h>                                                                                                                    
+#include <stdlib.h>
 #include "doomreplay.h"
 #include "doomgeneric.h"
 
 #include "doom.h"
 
-void D_DoomMain (void);                                                                                                                
+void D_DoomMain (void);
 void dg_Create();
 
 //#define USER_INPUT_LENGTH 118
 //char USER_INPUT[118] = ",,,,,,,,,,,,,,,,,,,,,,,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,r,r,r,r,f,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,f,f,f,f,f,f";
 
 // size of input = 1166
-char USER_INPUT[] = 
+char USER_INPUT[] =
 "u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,"
 "u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,"
 "r,r,r,r,r,r,r,r,r,r,r,r,"
@@ -70,7 +72,7 @@ char USER_INPUT[] =
 
 __attribute__((circuit)) int z_main(int n_inputs, int m)
 {
-    
+
     replay_data_t replay_data;
 
     replay_data.framerate = 35;
@@ -86,9 +88,9 @@ __attribute__((circuit)) int z_main(int n_inputs, int m)
             replay_data.n_frames++;
         }
     }
-    
+
     replay_data.frames    = malloc(replay_data.n_frames*sizeof(frame_data_t));
-    
+
     for (int f = 0; f < replay_data.n_frames; ++f) {
         for (int i = 0; i < dr_key_COUNT; ++i) {
             replay_data.frames[f].pressed[i] = 0;
@@ -129,22 +131,22 @@ __attribute__((circuit)) int z_main(int n_inputs, int m)
                 case '9': frame->pressed[dr_key_9]            = 1; break;
             };
     }
-    
-    replay_data.n_record = cur_frame;
-    return cur_frame;
 
-    //DR_Init(replay_data);
+    replay_data.n_record = cur_frame;
+
+    DR_Init(replay_data);
+    return cur_frame;
 
     //dg_Create();
 
 	//D_DoomMain ();
-    
+
 }
 
 
 
 unsigned long prepare_inputs(char * input, unsigned char *input_codes) {
-    // string with inputs like 
+    // string with inputs like
     // ",,,,,,,,,,,,,,,,,,,,,,,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,r,r,r,r,f,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,f,f,f,f,f,f";
     unsigned long n_inputs = sizeof(input);
     for (int i = 0; i < n_inputs; ++i) {
@@ -185,5 +187,108 @@ int main(int argc, char **argv) {
     int a = z_main(sizeof(USER_INPUT),11);
     return 0;
 }
+
+
+*/
+
+
+
+
+
+
+
+
+#include <stdio.h>
+const unsigned int INT_MAX=0xffffffff;
+#define VVV 42
+
+__attribute__((circuit)) unsigned int Dijkstra(unsigned int st, unsigned int end) {
+
+     /*int GR[VVV][VVV] = {
+        {0, 1, 4, 0, 2, 0},
+        {0, 0, 0, 9, 0, 0},
+        {4, 0, 0, 7, 0, 0},
+        {0, 9, 7, 0, 0, 2},
+        {0, 0, 0, 0, 0, 8},
+        {0, 0, 0, 0, 0, 0}
+    };*/
+
+    // fill graph with values
+    unsigned int GR[VVV][VVV];
+    unsigned int randstate = 0;
+
+    // #define VVV 8
+    for(int i = 0; i < VVV; i++) {
+        for (int j = i; j < VVV; j++) {
+            //randstate = 1664525 * randstate + 1013904223; // common values used in linear congruential generators
+            randstate = 3 * randstate + 5; // test
+            if (i==j) {
+                GR[i][j] = 0;
+            } else {
+                GR[i][j] = randstate;
+                GR[j][i] = GR[i][j];
+            }
+        }
+    }
+    for(int i = 0; i < VVV; i++) {
+        // 1st node is connected to each others onlyt through node 0
+        if (i != 0) {
+            GR[1][i] = 0; 
+            GR[i][1] = 0;
+        }
+    }
+
+
+#ifndef __ZKLLVM__
+    for(int i = 0; i < VVV; i++) {
+        for (int j = 0; j < VVV; j++) {
+            printf("\t %03d", GR[i][j]);
+        }
+        printf("\n");
+    }
+#endif
+
+    // calculate "distance" between each node with another 
+    unsigned int distance[VVV], count, index, i, u;
+    unsigned int visited[VVV];
+    for (i=0; i<VVV; i++) {
+        distance[i]=INT_MAX;
+        visited[i]=0;
+    }
+    distance[st]=0;
+    for (count=0; count<VVV-1; count++) {
+        int min=INT_MAX;
+        for (i=0; i<VVV; i++)
+            if (!visited[i] && distance[i]<=min) {
+                min=distance[i]; index=i;
+            }
+        u=index;
+        visited[u]=1;
+
+        for (i=0; i<VVV; i++)
+            if (!visited[i] && GR[u][i] && distance[u]!=INT_MAX && distance[u] + GR[u][i] < distance[i])
+                distance[i]=distance[u]+GR[u][i];
+    }
+#ifndef __ZKLLVM__
+    printf("Cost from start %d to each other:\n", st);
+    for (i=0; i<VVV; i++) {
+        if (distance[i]!=INT_MAX) {
+            printf("\tfrom %d to %d: %d\n", st, i, distance[i]);
+        } else {
+            printf("\tfrom %d to %d: (blocked)\n", st, i);
+        }
+    }
+#endif
+    return distance[end];
+}
+
+int main(int argc, char **argv) {
+    unsigned int start = 2;
+    unsigned int end = 1;
+    int distance = Dijkstra(start, end);
+    printf("Distance from %d to %d: %d\n\n", start, end, distance);
+    return 0;
+}
+
 
 
