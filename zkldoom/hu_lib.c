dabfd606603ada104d4152c548cdaace0ba477a6
hz <hz@hz.com>
2023-10-16 18:06:23 +0200

    removed HUD and status ops

--- zkldoom/hu_lib.c
+++ zkldoom/hu_lib.c
@@ -16,7 +16,7 @@
 //
 
 
-#include <ctype.h>
+// #include <ctype.h>
 
 #include "doomdef.h"
 #include "doomkeys.h"
@@ -31,17 +31,12 @@
 // boolean : whether the screen is always erased
 #define noterased viewwindowx
 
-extern boolean	automapactive;	// in AM_map.c
-
 void HUlib_init(void)
 {
 }
 
 void HUlib_clearTextLine(hu_textline_t* t)
 {
-    t->len = 0;
-    t->l[0] = 0;
-    t->needsupdate = true;
 }
 
 void
@@ -52,11 +47,6 @@ HUlib_initTextLine
   patch_t**		f,
   int			sc )
 {
-    t->x = x;
-    t->y = y;
-    t->f = f;
-    t->sc = sc;
-    HUlib_clearTextLine(t);
 }
 
 boolean
@@ -64,30 +54,10 @@ HUlib_addCharToTextLine
 ( hu_textline_t*	t,
   char			ch )
 {
-
-    if (t->len == HU_MAXLINELENGTH)
-	return false;
-    else
-    {
-	t->l[t->len++] = ch;
-	t->l[t->len] = 0;
-	t->needsupdate = 4;
-	return true;
-    }
-
 }
 
 boolean HUlib_delCharFromTextLine(hu_textline_t* t)
 {
-
-    if (!t->len) return false;
-    else
-    {
-	t->l[--t->len] = 0;
-	t->needsupdate = 4;
-	return true;
-    }
-
 }
 
 void
@@ -96,73 +66,12 @@ HUlib_drawTextLine
   boolean		drawcursor )
 {
 
-    int			i;
-    int			w;
-    int			x;
-    unsigned char	c;
-
-    // draw the new stuff
-    x = l->x;
-    for (i=0;i<l->len;i++)
-    {
-	c = toupper((int)l->l[i]);
-	if (c != ' '
-	    && c >= l->sc
-	    && c <= '_')
-	{
-	    w = SHORT(l->f[c - l->sc]->width);
-	    if (x+w > SCREENWIDTH)
-		break;
-	    V_DrawPatchDirect(x, l->y, l->f[c - l->sc]);
-	    x += w;
-	}
-	else
-	{
-	    x += 4;
-	    if (x >= SCREENWIDTH)
-		break;
-	}
-    }
-
-    // draw the cursor if requested
-    if (drawcursor
-	&& x + SHORT(l->f['_' - l->sc]->width) <= SCREENWIDTH)
-    {
-	V_DrawPatchDirect(x, l->y, l->f['_' - l->sc]);
-    }
 }
 
 
 // sorta called by HU_Erase and just better darn get things straight
 void HUlib_eraseTextLine(hu_textline_t* l)
 {
-    int			lh;
-    int			y;
-    int			yoffset;
-
-    // Only erases when NOT in automap and the screen is reduced,
-    // and the text must either need updating or refreshing
-    // (because of a recent change back from the automap)
-
-    if (!automapactive &&
-	viewwindowx && l->needsupdate)
-    {
-	lh = SHORT(l->f[0]->height) + 1;
-	for (y=l->y,yoffset=y*SCREENWIDTH ; y<l->y+lh ; y++,yoffset+=SCREENWIDTH)
-	{
-	    if (y < viewwindowy || y >= viewwindowy + viewheight)
-		R_VideoErase(yoffset, SCREENWIDTH); // erase entire line
-	    else
-	    {
-		R_VideoErase(yoffset, viewwindowx); // erase left border
-		R_VideoErase(yoffset + viewwindowx + viewwidth, viewwindowx);
-		// erase right border
-	    }
-	}
-    }
-
-    if (l->needsupdate) l->needsupdate--;
-
 }
 
 void
@@ -175,34 +84,10 @@ HUlib_initSText
   int		startchar,
   boolean*	on )
 {
-
-    int i;
-
-    s->h = h;
-    s->on = on;
-    s->laston = true;
-    s->cl = 0;
-    for (i=0;i<h;i++)
-	HUlib_initTextLine(&s->l[i],
-			   x, y - i*(SHORT(font[0]->height)+1),
-			   font, startchar);
-
 }
 
 void HUlib_addLineToSText(hu_stext_t* s)
 {
-
-    int i;
-
-    // add a clear line
-    if (++s->cl == s->h)
-	s->cl = 0;
-    HUlib_clearTextLine(&s->l[s->cl]);
-
-    // everything needs updating
-    for (i=0 ; i<s->h ; i++)
-	s->l[i].needsupdate = 4;
-
 }
 
 void
@@ -211,51 +96,14 @@ HUlib_addMessageToSText
   char*		prefix,
   char*		msg )
 {
-    HUlib_addLineToSText(s);
-    if (prefix)
-	while (*prefix)
-	    HUlib_addCharToTextLine(&s->l[s->cl], *(prefix++));
-
-    while (*msg)
-	HUlib_addCharToTextLine(&s->l[s->cl], *(msg++));
 }
 
 void HUlib_drawSText(hu_stext_t* s)
 {
-    int i, idx;
-    hu_textline_t *l;
-
-    if (!*s->on)
-	return; // if not on, don't draw
-
-    // draw everything
-    for (i=0 ; i<s->h ; i++)
-    {
-	idx = s->cl - i;
-	if (idx < 0)
-	    idx += s->h; // handle queue of lines
-	
-	l = &s->l[idx];
-
-	// need a decision made here on whether to skip the draw
-	HUlib_drawTextLine(l, false); // no cursor, please
-    }
-
 }
 
 void HUlib_eraseSText(hu_stext_t* s)
 {
-
-    int i;
-
-    for (i=0 ; i<s->h ; i++)
-    {
-	if (s->laston && !*s->on)
-	    s->l[i].needsupdate = 4;
-	HUlib_eraseTextLine(&s->l[i]);
-    }
-    s->laston = *s->on;
-
 }
 
 void
@@ -267,31 +115,21 @@ HUlib_initIText
   int		startchar,
   boolean*	on )
 {
-    it->lm = 0; // default left margin is start of text
-    it->on = on;
-    it->laston = true;
-    HUlib_initTextLine(&it->l, x, y, font, startchar);
 }
 
 
 // The following deletion routines adhere to the left margin restriction
 void HUlib_delCharFromIText(hu_itext_t* it)
 {
-    if (it->l.len != it->lm)
-	HUlib_delCharFromTextLine(&it->l);
 }
 
 void HUlib_eraseLineFromIText(hu_itext_t* it)
 {
-    while (it->lm != it->l.len)
-	HUlib_delCharFromTextLine(&it->l);
 }
 
 // Resets left margin as well
 void HUlib_resetIText(hu_itext_t* it)
 {
-    it->lm = 0;
-    HUlib_clearTextLine(&it->l);
 }
 
 void
@@ -299,9 +137,6 @@ HUlib_addPrefixToIText
 ( hu_itext_t*	it,
   char*		str )
 {
-    while (*str)
-	HUlib_addCharToTextLine(&it->l, *(str++));
-    it->lm = it->l.len;
 }
 
 // wrapper function for handling general keyed input.
@@ -311,37 +146,14 @@ HUlib_keyInIText
 ( hu_itext_t*	it,
   unsigned char ch )
 {
-    ch = toupper(ch);
-
-    if (ch >= ' ' && ch <= '_') 
-  	HUlib_addCharToTextLine(&it->l, (char) ch);
-    else 
-	if (ch == KEY_BACKSPACE) 
-	    HUlib_delCharFromIText(it);
-	else 
-	    if (ch != KEY_ENTER) 
-		return false; // did not eat key
-
     return true; // ate the key
-
 }
 
 void HUlib_drawIText(hu_itext_t* it)
 {
-
-    hu_textline_t *l = &it->l;
-
-    if (!*it->on)
-	return;
-    HUlib_drawTextLine(l, true); // draw the line w/ cursor
-
 }
 
 void HUlib_eraseIText(hu_itext_t* it)
 {
-    if (it->laston && !*it->on)
-	it->l.needsupdate = 4;
-    HUlib_eraseTextLine(&it->l);
-    it->laston = *it->on;
 }
 
--- zkldoom/st_lib.c
+++ zkldoom/st_lib.c
@@ -17,8 +17,8 @@
 //
 
 
-#include <stdio.h>
-#include <ctype.h>
+//#include <stdio.h>
+//#include <ctype.h>
 
 #include "deh_main.h"
 #include "doomdef.h"
@@ -37,9 +37,6 @@
 
 
 // in AM_map.c
-extern boolean		automapactive; 
-
-
 
 
 //
@@ -50,7 +47,6 @@ patch_t*		sttminus;
 
 void STlib_init(void)
 {
-    sttminus = (patch_t *) W_CacheLumpName(DEH_String("STTMINUS"), PU_STATIC);
 }
 
 
@@ -65,13 +61,6 @@ STlib_initNum
   boolean*		on,
   int			width )
 {
-    n->x	= x;
-    n->y	= y;
-    n->oldnum	= 0;
-    n->width	= width;
-    n->num	= num;
-    n->on	= on;
-    n->p	= pl;
 }
 
 
@@ -85,59 +74,6 @@ STlib_drawNum
 ( st_number_t*	n,
   boolean	refresh )
 {
-
-    int		numdigits = n->width;
-    int		num = *n->num;
-    
-    int		w = SHORT(n->p[0]->width);
-    int		h = SHORT(n->p[0]->height);
-    int		x = n->x;
-    
-    int		neg;
-
-    n->oldnum = *n->num;
-
-    neg = num < 0;
-
-    if (neg)
-    {
-	if (numdigits == 2 && num < -9)
-	    num = -9;
-	else if (numdigits == 3 && num < -99)
-	    num = -99;
-	
-	num = -num;
-    }
-
-    // clear the area
-    x = n->x - numdigits*w;
-
-    if (n->y - ST_Y < 0)
-	I_Error("drawNum: n->y - ST_Y < 0");
-
-    V_CopyRect(x, n->y - ST_Y, st_backing_screen, w*numdigits, h, x, n->y);
-
-    // if non-number, do not draw it
-    if (num == 1994)
-	return;
-
-    x = n->x;
-
-    // in the special case of 0, you draw 0
-    if (!num)
-	V_DrawPatch(x - w, n->y, n->p[ 0 ]);
-
-    // draw the new number
-    while (num && numdigits--)
-    {
-	x -= w;
-	V_DrawPatch(x, n->y, n->p[ num % 10 ]);
-	num /= 10;
-    }
-
-    // draw a minus sign if necessary
-    if (neg)
-	V_DrawPatch(x - 8, n->y, sttminus);
 }
 
 
@@ -147,7 +83,6 @@ STlib_updateNum
 ( st_number_t*		n,
   boolean		refresh )
 {
-    if (*n->on) STlib_drawNum(n, refresh);
 }
 
 
@@ -162,8 +97,6 @@ STlib_initPercent
   boolean*		on,
   patch_t*		percent )
 {
-    STlib_initNum(&p->n, x, y, pl, num, on, 3);
-    p->p = percent;
 }
 
 
@@ -174,10 +107,6 @@ STlib_updatePercent
 ( st_percent_t*		per,
   int			refresh )
 {
-    if (refresh && *per->n.on)
-	V_DrawPatch(per->n.x, per->n.y, per->p);
-    
-    STlib_updateNum(&per->n, refresh);
 }
 
 
@@ -191,12 +120,6 @@ STlib_initMultIcon
   int*			inum,
   boolean*		on )
 {
-    i->x	= x;
-    i->y	= y;
-    i->oldinum 	= -1;
-    i->inum	= inum;
-    i->on	= on;
-    i->p	= il;
 }
 
 
@@ -206,28 +129,6 @@ STlib_updateMultIcon
 ( st_multicon_t*	mi,
   boolean		refresh )
 {
-    int			w;
-    int			h;
-    int			x;
-    int			y;
-
-    if (*mi->on && (mi->oldinum != *mi->inum || refresh) && (*mi->inum != -1))
-    {
-	if (mi->oldinum != -1)
-	{
-	    x = mi->x - SHORT(mi->p[mi->oldinum]->leftoffset);
-	    y = mi->y - SHORT(mi->p[mi->oldinum]->topoffset);
-	    w = SHORT(mi->p[mi->oldinum]->width);
-	    h = SHORT(mi->p[mi->oldinum]->height);
-
-	    if (y - ST_Y < 0)
-		I_Error("updateMultIcon: y - ST_Y < 0");
-
-	    V_CopyRect(x, y-ST_Y, st_backing_screen, w, h, x, y);
-	}
-	V_DrawPatch(mi->x, mi->y, mi->p[*mi->inum]);
-	mi->oldinum = *mi->inum;
-    }
 }
 
 
@@ -241,12 +142,6 @@ STlib_initBinIcon
   boolean*		val,
   boolean*		on )
 {
-    b->x	= x;
-    b->y	= y;
-    b->oldval	= false;
-    b->val	= val;
-    b->on	= on;
-    b->p	= i;
 }
 
 
@@ -256,29 +151,5 @@ STlib_updateBinIcon
 ( st_binicon_t*		bi,
   boolean		refresh )
 {
-    int			x;
-    int			y;
-    int			w;
-    int			h;
-
-    if (*bi->on
-     && (bi->oldval != *bi->val || refresh))
-    {
-	x = bi->x - SHORT(bi->p->leftoffset);
-	y = bi->y - SHORT(bi->p->topoffset);
-	w = SHORT(bi->p->width);
-	h = SHORT(bi->p->height);
-
-	if (y - ST_Y < 0)
-	    I_Error("updateBinIcon: y - ST_Y < 0");
-
-	if (*bi->val)
-	    V_DrawPatch(bi->x, bi->y, bi->p);
-	else
-	    V_CopyRect(x, y-ST_Y, st_backing_screen, w, h, x, y);
-
-	bi->oldval = *bi->val;
-    }
-
 }
 
